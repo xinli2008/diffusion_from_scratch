b4f272ae0778d75faba60c367ffe9937fd129348
xinli2008 <czu_lixin@163.com>
2025-03-31 23:01:42 +0800

    update inference code

--- inference.py
+++ inference.py
@@ -3,10 +3,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
-from dataset import MNISTDataset
+from dataset import CustomDataset
 from diffusion.ddpm import DDPMScheduler
 from models.unet import UnetModel
+from models.dit import DiT_models
+from models.uvit import Uvit_models
 import argparse
 from tqdm import tqdm
 from PIL import Image
@@ -18,19 +19,26 @@ tensor_to_pil = transforms.Compose([
      transforms.ToPILImage(),
  ])
 
+def get_dataset_channels(dataset_name):
+    channels = {
+        "MNIST": 1,
+        "CIFAR-10": 3,
+    }
+    return channels.get(dataset_name)
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Test a mini diffusion model")
 
     # Model parameters
-    parser.add_argument("--model_arch", type=str, default="unet", choices=["unet", "dit", "uvit"],
+    parser.add_argument("--model_arch", type=str, default="dit", choices=["unet", "dit", "uvit"],
                         help="Architecture of the model to use. ")
-    parser.add_argument("--in_channels", type=int, default=1, 
-                        help="Number of input channels (e.g., 1 for grayscale images, 3 for RGB). Default is 1.")
-    parser.add_argument("--image_size", type=int, default=64, 
+    parser.add_argument("--image_size", type=int, default=32, 
                         help="Size of the input images (height and width). Default is 64.")
-    parser.add_argument("--model_load_dir", type=str, default="/data/lixin/practice/diffusion_from_scratch/results/NNIST_train_20250310/model/model_epoch_900.pt",
+    parser.add_argument("--model_load_dir", type=str, default="/data/lixin/practice/diffusion_from_scratch/results/CIFAR_train_20250330222/model/model_epoch_1100.pt",
                     help="Directory to load the model weights from")
-    parser.add_argument("--output_dir", type=str, default="/data/lixin/output",
+    parser.add_argument("--dataset_name", type=str, default="CIFAR-10", choices=["MNIST", "CIFAR-10"], 
+                        help="Name of the dataset to use. Default is 'MNIST'.")
+    parser.add_argument("--output_dir", type=str, default="/data/lixin/practice/diffusion_from_scratch/output",
                         help="Directory to save the output images")
     parser.add_argument("--num_images", type=int, default=20, help="Number of images to generate")
     parser.add_argument("--batch_size", type=int, default=10, help="Number of images to generate")
@@ -81,12 +89,16 @@ def save_combined_image(tensors, path, grid_size, img_size):
 if __name__ == "__main__":
     args = parse_args()
 
+    in_channels = get_dataset_channels(args.dataset_name)
+    if in_channels is None:
+        raise ValueError(f"Unexpected dataset type: {args.dataset_name}")
+
     if args.model_arch == "unet":
-        model = UnetModel(args.in_channels).to(args.device)
+        model = UnetModel(1).to(args.device)
     elif args.model_arch == "dit":
-        pass
+        model = DiT_models["DiT-L/8"](image_size = (args.image_size, args.image_size), input_channel = in_channels, num_labels = 10).to(args.device)
     elif args.model_arch == "uvit":
-        pass
+        model = Uvit_models["UVit_base"](img_size = (args.image_size, args.image_size), in_channels = in_channels, num_classes = 10).to(args.device)
     else:
         raise ValueError(f"unexpected model architecture for {args.model_arch}")
     
@@ -95,6 +107,7 @@ if __name__ == "__main__":
     
     model_weights = torch.load(args.model_load_dir)
     missing_keys, unexcepted_keys = model.load_state_dict(model_weights, strict = True)
+    model = model.eval()
     print(f"succeed to load model weights with {len(missing_keys)} missing keys and {len(unexcepted_keys)} unexcepted keys")
 
     diffusion_scheduler = DDPMScheduler(args.beta_schedule, args.noise_steps, args.beta_start, args.beta_end, args.cosine_s)
@@ -105,27 +118,25 @@ if __name__ == "__main__":
     output_dir = args.output_dir
     os.makedirs(output_dir, exist_ok=True)
 
-    batch_x_t = torch.randn(size=(batch_size, 1, image_size, image_size)).to(args.device)
+    batch_x_t = torch.randn(size=(batch_size, 3, image_size, image_size)).to(args.device)
     batch_cls = torch.arange(start = 0, end = 10, dtype = torch.long).to(args.device)
     denoised_images = diffusion_scheduler.denoise_sample(model, batch_x_t, batch_cls)
 
-    # 保存每个批次的汇总图像
     combined_images = []
     for b in range(batch_size):
         img_list = []
         for i in range(0, num_imgs):
-            idx = int(1000 / num_imgs) * (i + 1)
-            # 像素值还原到 [0, 1]
-            final_img = (denoised_images[idx][b].to('cpu') + 1) / 2
+            # idx = int(1000 / num_imgs) * (i + 1)
+            idx = 500 + int(500 * i / (num_imgs - 1)) 
+            idx = min(idx, args.noise_steps - 1)
+            final_img = (denoised_images[idx][b].to('cpu') + 1) /2
             img_list.append(final_img)
 
-        # 保存汇总图像
         grid_size = (1, num_imgs)
         img_path = os.path.join(output_dir, f"batch_{b}_combined.png")
         save_combined_image(img_list, img_path, grid_size, (image_size, image_size))
         combined_images.append(img_path)
 
-    # 拼接所有汇总图像
     final_image_width = image_size * num_imgs
     final_image_height = image_size * batch_size
     final_image = Image.new('RGB', (final_image_width, final_image_height))
@@ -134,6 +145,5 @@ if __name__ == "__main__":
         img = Image.open(img_path)
         final_image.paste(img, (0, i * image_size))
 
-    # 保存最终的大图像
     final_image_path = os.path.join(output_dir, "final_combined_image.png")
     final_image.save(final_image_path)
